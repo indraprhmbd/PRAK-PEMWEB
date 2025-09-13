896735347a30f6936c32023d63bca0a2d16284e1
indraprhmbd <indraprhmbd@gmail.com>
2025-09-13 21:06:47 +0700

    update form-daftar : completing the logic for alert succes and danger, update index.html : none

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 const body = document.body;
 const formLink = document.getElementsByClassName('form-link')
+const mybutton = document.getElementById("btn-back-to-top");
 
 btn = document.getElementById('btn');
 
+// get the button
+
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+// when clicked, scroll to top
+mybutton.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
