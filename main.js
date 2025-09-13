2727af6c961c34c72d37472ec82f5bc75038df57
indraprhmbd <indraprhmbd@gmail.com>
2025-09-13 21:07:31 +0700

    update form-daftar : completing the logic for alert succes and danger, update index.html : none

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
 const body = document.body;
-const formLink = document.getElementsByClassName('form-link')
+const formLink = document.getElementsByClassName("form-link");
 const mybutton = document.getElementById("btn-back-to-top");
+const alert = document.getElementsByClassName("alert");
+const alertSuccess = document.getElementById("successAlert");
+const alertDanger = document.getElementById("dangerAlert");
+const submitForm = document.getElementById("submitBtn");
+const checkBox = document.getElementById("checkbox");
+const form = document.getElementById("myForm");
+btn = document.getElementById("btn");
 
-btn = document.getElementById('btn');
+form.addEventListener("submit", function (event) {
+  const inputs = form.querySelectorAll("input");
+  let allFilled = true;
 
-// get the button
+  inputs.forEach((input) => {
+    if (input.value.trim() === "") {
+      allFilled = false;
+    }
+  });
 
+  if (!(allFilled && checkBox.checked)) {
+    event.preventDefault();
+    alertDanger.style.display = "block";
+  } else {
+    event.preventDefault();
+    alertSuccess.style.display = "block";
+    form.reset();
+    alertDanger.style.display = "none";
+  }
+  
+  window.scrollTo({ top: 0, behavior: "smooth" });
+
+  setTimeout(() => {
+    alertSuccess.style.display = "none";
+  }, 5000);
+});
 
 window.onscroll = function () {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -17,5 +49,5 @@ window.onscroll = function () {
 
 // when clicked, scroll to top
 mybutton.addEventListener("click", function () {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
