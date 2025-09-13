b82b258afaafad4a3255820e462922b0dff83a21
indraprhmbd <indraprhmbd@gmail.com>
2025-09-13 22:17:52 +0700

    update main.js : fixing error on 'btn-back-to-top' because of unguarded null addEventListener

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const body = document.body;
 const formLink = document.getElementsByClassName("form-link");
 const mybutton = document.getElementById("btn-back-to-top");
 const alert = document.getElementsByClassName("alert");
@@ -7,47 +6,52 @@ const alertDanger = document.getElementById("dangerAlert");
 const submitForm = document.getElementById("submitBtn");
 const checkBox = document.getElementById("checkbox");
 const form = document.getElementById("myForm");
-btn = document.getElementById("btn");
 
-form.addEventListener("submit", function (event) {
-  const inputs = form.querySelectorAll("input");
-  let allFilled = true;
+if(form){
+    form.addEventListener("submit", function (event) {
+    const inputs = form.querySelectorAll("input");
+    let allFilled = true;
 
-  inputs.forEach((input) => {
-    if (input.value.trim() === "") {
-      allFilled = false;
+    inputs.forEach((input) => {
+      if (input.value.trim() === "") {
+        allFilled = false;
+      }
+    });
+
+    if (!(allFilled && checkBox.checked)) {
+      event.preventDefault();
+      alertDanger.style.display = "block";
+      setTimeout(() => {
+        alertDanger.style.display = "none";
+      }, 3000);
+    } else {
+      event.preventDefault();
+      alertSuccess.style.display = "block";
+      form.reset();
+      alertDanger.style.display = "none";
+      setTimeout(() => {
+        alertSuccess.style.display = "none";
+      }, 5000);
     }
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
   });
+}
 
-  if (!(allFilled && checkBox.checked)) {
-    event.preventDefault();
-    alertDanger.style.display = "block";
-  } else {
-    event.preventDefault();
-    alertSuccess.style.display = "block";
-    form.reset();
-    alertDanger.style.display = "none";
-  }
-  
-  window.scrollTo({ top: 0, behavior: "smooth" });
+if (mybutton) {
+  window.onscroll = function () {
+    if (
+      document.body.scrollTop > 200 ||
+      document.documentElement.scrollTop > 200
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  };
 
-  setTimeout(() => {
-    alertSuccess.style.display = "none";
-  }, 5000);
-});
-
-window.onscroll = function () {
-  if (
-    document.body.scrollTop > 200 ||
-    document.documentElement.scrollTop > 200
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-};
-
-// when clicked, scroll to top
-mybutton.addEventListener("click", function () {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
+  // when clicked, scroll to top
+  mybutton.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+}
